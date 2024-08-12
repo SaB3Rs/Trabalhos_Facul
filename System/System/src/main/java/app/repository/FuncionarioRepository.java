6cf43b39038b00a5c75ca351a407997415b8b450
Kaiky Belgoni <131906646+SaB3Rs@users.noreply.github.com>
2024-08-11 23:46:58 -0300

    Delete System/System directory

--- System/System/src/main/java/app/repository/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import app.entity.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-
-}
